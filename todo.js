10017d59c64d511bfc51b2c615ec5c3b676ea16c
shashank-kulkarni9 <101931833+shashank-kulkarni9@users.noreply.github.com>
2022-11-21 11:42:00 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,80 @@
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    var i;
+    var l = [];
+    for (i in all) {
+      if (all[i].dueDate < today) {
+        l.push(all[i]);
+      }
+    }
+    return l;
+  };
+
+  const dueToday = () => {
+    var i;
+    var l = [];
+    for (i in all) {
+      if (all[i].dueDate === today) {
+        l.push(all[i]);
+      }
+    }
+    return l;
+  };
+
+  const dueLater = () => {
+    var i;
+    var l = [];
+    for (i in all) {
+      if (all[i].dueDate > today) {
+        l.push(all[i]);
+      }
+    }
+    return l;
+  };
+
+  const toDisplayableList = (list) => {
+    var i;
+    var final_list = [];
+    for (i = 0; i < list.length; i++) {
+      if (list[i].completed === false) {
+        if (list[i].dueDate === today) {
+          final_list.push(`[ ] ${list[i].title}`);
+        } else {
+          final_list.push(`[ ] ${list[i].title} ${list[i].dueDate}`);
+        }
+      } else {
+        if (list[i].dueDate === today) {
+          final_list.push(`[x] ${list[i].title}`);
+        } else {
+          final_list.push(`[x] ${list[i].title} ${list[i].dueDate}`);
+        }
+      }
+    }
+
+    return final_list.join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+module.exports = todoList;
